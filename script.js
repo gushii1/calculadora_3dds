288ead0ee63c77a1b45c264917325f0c60ef834e
gushii1 <167936108+gushii1@users.noreply.github.com>
2025-04-03 19:41:36 -0300

    rafaela linda

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let resultado = document.getElementById('resultado');
+let expressao = '';
+
+function adicionar(valor) {
+  expressao += valor;
+  resultado.value = expressao;
+}
+
+function adicionarOperador(operador) {
+  if (expressao.length > 0 && !isNaN(expressao[expressao.length - 1])) {
+    expressao += operador;
+    resultado.value = expressao;
+  }
+}
+
+function calcular() {
+  try {
+    expressao = eval(expressao).toString();
+    resultado.value = expressao;
+  } catch (e) {
+    resultado.value = 'Erro';
+    expressao = '';
+  }
+}
+
+function limpar() {
+  expressao = '';
+  resultado.value = '';
+}
+
 
